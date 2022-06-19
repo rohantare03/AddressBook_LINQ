d7514fc5400133780bf29239cadae12d78263fc7
rohantare03 <rohatare123@gmail.com>
2022-06-19 19:07:41 +0530

    [Rohan] Add : Creating Address Book Data Table

--- AddressBookLinq/AddressBookLinq/AddressBook.cs
+++ AddressBookLinq/AddressBookLinq/AddressBook.cs
@@ -9,9 +9,17 @@ namespace AddressBookLinq
 {
     public class AddressBook
     {
+        DataTable dataTable = new DataTable();
         public void AddressBookDataTable()
-        {
-            DataTable dataTable = new DataTable();
+        {          
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+            dataTable.Columns.Add("Address", typeof(string));
+            dataTable.Columns.Add("City", typeof(string));
+            dataTable.Columns.Add("State", typeof(string));
+            dataTable.Columns.Add("Zip", typeof(int));
+            dataTable.Columns.Add("PhoneNumber", typeof(long));
+            dataTable.Columns.Add("Email", typeof(string));
         }
     }
 }
