54e6057165fa5f30d1c98081fc2f41bfbf3ecc1c
rohantare03 <rohatare123@gmail.com>
2022-06-19 19:01:40 +0530

    [Rohan] Add : Creating Data Table

---
+++ AddressBookLinq/AddressBookLinq/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookLinq
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Using LINQ");
+        }
+    }
+}
